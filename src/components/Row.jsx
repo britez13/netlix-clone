a3c69b64527a084640686363b12a9380784544d1
Florencio Britez <florenciobritez49@gmail.com>
2022-10-03 23:51:19 -0300

    Initialize firebase app

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,38 +1,47 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md"
+import Movie from "./Movie";
 
-const Row = ({ title, fetchURL }) => {
+const Row = ({ title, fetchURL, rowID }) => {
   const [movies, setMovies] = useState();
 
   useEffect(() => {
     axios.get(fetchURL).then((res) => setMovies(res.data.results));
   }, [fetchURL]);
 
-  console.log(movies);
+  const slideLeft = () => {
+    var slider = document.getElementById("slider" + rowID);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  }
+
+  const slideRight = () => { 
+    var slider = document.getElementById("slider" + rowID);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  }
 
   return (
     <>
       <h2 className='text-white font-bold md:text-xl py-4'>{title}</h2>
-      <div className='relative flex items-center'>
-        <div id={"slider"}>
+      <div className='relative flex items-center group'>
+        <MdChevronLeft
+          onClick={slideLeft}
+          size={40}
+          className='bg-white rounded-full absolute left-0 opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+        />
+        <div
+          id={"slider" + rowID}
+          className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'
+        >
           {movies?.map((item) => {
-            return (
-              <div
-                key={item.id}
-                className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative py-2'
-              >
-                <img
-                  className='w-full h-auto block'
-                  src={`https://image.tmdb.org/t/p/w500/${item?.poster_path}`}
-                  alt={item?.title}
-                />
-                <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
-                  <p>{item?.title}</p>
-                </div>
-              </div>
-            );
+            return <Movie key={item.id} item={item} />;
           })}
         </div>
+        <MdChevronRight
+          onClick={slideRight}
+          size={40}
+          className='bg-white rounded-full absolute right-0 opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+        />
       </div>
     </>
   );
